97a98de97cf5f17f0b628dfb5029441517a6415a
MBoota <m43577535@gmail.com>
2024-02-04 15:14:18 +0500

    coverting both controller multiple methods default action

--- Controll_ModelBinding_otherConcepts/Controllers/HomeController.cs
+++ Controll_ModelBinding_otherConcepts/Controllers/HomeController.cs
@@ -2,12 +2,34 @@
 
 namespace Controll_ModelBinding_otherConcepts.Controllers
 {
+    [Controller]
     public class HomeController
     {
-        [Route("Pakistan")]
+        //=============================================
+        //_______ 1. Adding Controller Action __________
+        //=============================================
+        [Route("Pakistan1")]
+        [Route("Pakistan2")]
         public string ListRecords()
         {
             return "Pakistan";
         }
+
+        [Route("ContactUs/{id}")]
+        public string ContactUs()
+        {
+            return "This is HomePage";
+        }
+
+        //=============================================
+        //--------- 2. Adding Default Aciton ---------
+        //=============================================
+        [Route("/")]
+        public string Index()
+        {
+            return "This is HomePage";
+        }
+
+        
     }
 }
