b4955d68a5e86d771035d3e627528f084f6f6251
MBoota <m43577535@gmail.com>
2024-02-04 18:15:33 +0500

    RedirctToAction and other method

---
+++ Controll_ModelBinding_otherConcepts/Controllers/RedirectController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace Controll_ModelBinding_otherConcepts.Controllers
+{
+    public class RedirectController : Controller
+    {
+        //========================================
+        //--------- RedirectToAction -------------
+        //========================================
+        public IActionResult Index()
+        {
+            //return new RedirectToActionResult("Index", "StatusCode", new { bookId = 14, isLoggedIn = true });
+            return RedirectToAction("Index", "StatusCode",new { bookId = 14 , isLoggedIn =true});
+        }
+
+        //========================================
+        //--------- LocalRedirect -------------
+        //========================================
+        public IActionResult Index2()
+        {
+            return LocalRedirect("~/StatusCode/Index?bookId=14&isLoggedIn=true");
+        }
+
+        //========================================
+        //--------- RedirectToActionPermanent -------------
+        //========================================
+        public IActionResult Index1()
+        {
+            return RedirectToActionPermanent("Index", "StatusCode", new { bookId = 14, isLoggedIn = true });
+        }
+
+        
+
+    }
+}
