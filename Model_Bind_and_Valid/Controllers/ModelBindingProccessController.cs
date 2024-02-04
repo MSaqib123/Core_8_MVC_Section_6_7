86c266998ca7674695b8fa88e34cf2202a53e7b0
MBoota <m43577535@gmail.com>
2024-02-04 20:17:40 +0500

    Model Binding , QueryString ,  RouteData

---
+++ Model_Bind_and_Valid/Controllers/ModelBindingProccessController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace Controll_ModelBinding_otherConcepts.Controllers
+{
+    [Controller]
+    public class ModelBindingProccessController:Controller
+    {
+        //=============================================
+        //_______ ModelBindingProccess __________
+        //=============================================
+
+        #region ModelBindingProccess
+        [Route("Book")]
+        public IActionResult Index(int bookid,bool isLoggedIn)
+        {
+            if (bookid == null)
+            {
+                return BadRequest("Book id is not supplied");
+            }
+
+            //book id Can't be empty
+            if (bookid == null)
+            {
+                return BadRequest("Book id cannot be empty");
+            }
+            //int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
+            int bookId = bookid;
+            if (bookId <= 0)
+            {
+                return BadRequest("Book id cant't be less then or eques to 0");
+            }
+            if (bookId >= 1000)
+            {
+                return BadRequest("Book id cant't be greter then or eques to 1000");
+            }
+
+            //check Login or not
+            //if (Convert.ToBoolean(Request.Query["isLoggedIn"])==false)
+            if (isLoggedIn == false)
+            {
+                return StatusCode(401);
+            }
+
+            return File("/sample.pdf","applicaton/pdf");
+        }
+        
+        #endregion
+
+
+    }
+}
--- Model_Bind_and_Valid/Program.cs
+++ Model_Bind_and_Valid/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//___ Adding Controller Middleware ___
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
