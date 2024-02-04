eb1ea38fb007ceac6d449ebc86086ca88312bcb6
MBoota <m43577535@gmail.com>
2024-02-04 15:02:13 +0500

    Adding Controller and REquest

---
+++ Controll_ModelBinding_otherConcepts/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controll_ModelBinding_otherConcepts.Controllers
+{
+    public class HomeController
+    {
+        [Route("Pakistan")]
+        public string ListRecords()
+        {
+            return "Pakistan";
+        }
+    }
+}
--- Controll_ModelBinding_otherConcepts/Program.cs
+++ Controll_ModelBinding_otherConcepts/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//=====================================
+//=========== Controllers ============
+//=====================================
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//___ Adding Controller Middleware ___
+app.UseRouting();
+app.MapControllers();
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
