2864c5fdff95b086d6adfd2b82cd6214d85885ab
MBoota <m43577535@gmail.com>
2024-02-05 13:08:10 +0500

    FromDate > toDate Multiple Custom Validation here

---
+++ Model_Bind_and_Valid/CustomValidators/DateRangeValidatorAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Model_Bind_and_Valid.CustomValidators
+{
+    public class DateRangeValidatorAttribute : ValidationAttribute
+    {
+        public string OtherPropertyName { get; set; }
+
+        #region Constructor
+        //public DateRangeValidatorAttribute() { }    in this paremter is requed
+        public DateRangeValidatorAttribute(string otherPropertyName)
+        {
+            OtherPropertyName = otherPropertyName;
+        }
+        #endregion
+
+        #region IsValid
+        protected override ValidationResult? IsValid(
+            object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                //_____ currentProperty from_date ____
+                DateTime to_date = (DateTime)value;
+
+                //_____ otherProperty to_date ____
+                //we have name of otherProperty
+                //but  value will come from DataBinding(Context) by (Reflection) concept
+                //Reflection is used Read MataData from object dynamicaly
+                PropertyInfo? otherProperty = validationContext.ObjectType.GetProperty(OtherPropertyName);
+                //DateTime from_date = Convert.ToDateTime(otherProperty.GetValue(validationContext.ObjectInstance));
+
+                if (otherProperty != null)
+                {
+                    DateTime from_date = Convert.ToDateTime(otherProperty.GetValue(validationContext.ObjectInstance));
+                    if (from_date > to_date)
+                    {
+                        return new ValidationResult(
+                            ErrorMessage,
+                            new string[] {
+                            OtherPropertyName,//"FromDate",
+                            validationContext.MemberName//"ToDate"
+                            }
+                          );
+                    }
+                    else
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+                return null;
+            }
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ Model_Bind_and_Valid/Models/MultiCustomValidationModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Model_Bind_and_Valid.CustomValidators;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model_Bind_and_Valid.Models
+{
+    public class MultiCustomValidationModel
+    {
+        //________________ CustomValidation _____________
+        //[MinimumYearValidator(2000, ErrorMessage = "Minimum year allowed is 2000")]
+        //[MinimumYearValidator(2010, ErrorMessage = "Minimum year allowed is {0}")]
+        [MinimumYearValidator(2016)]
+        public DateTime? DOB { get; set; }
+
+        //________________ MultiCustomValidation _____________
+        public DateTime? FromDate { get; set; }
+        [DateRangeValidator("FromDate",ErrorMessage="'FromDate' olderthen or equal to 'ToDate'")]
+        public DateTime? ToDate { get; set; }
+    }
+}
